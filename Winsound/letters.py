ca0843814e9e80f05e01111afb4c67cbb7bd8ecd
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-12-31 10:21:51 +0000

    Create letters.py

---
+++ Winsound/letters.py
@@ -0,0 +1,33 @@
+import winsound
+
+def H():  # ....
+    for i in range(4):
+        winsound.Beep(380, 500)
+
+def E():  # .
+    winsound.Beep(380, 500)
+
+def L():  # .-..
+    winsound.Beep(380, 500)
+    winsound.Beep(380, 1000)
+    winsound.Beep(380, 500)
+    winsound.Beep(380, 500)
+
+def O():  # ---
+    for i in range(3):
+        winsound.Beep(380, 1000)
+
+def W():  # .--
+    winsound.Beep(380, 500)
+    winsound.Beep(380, 1000)
+    winsound.Beep(380, 1000)
+
+def R():  # .-.
+    winsound.Beep(380, 500)
+    winsound.Beep(380, 1000)
+    winsound.Beep(380, 500)
+
+def D():  # -..
+    winsound.Beep(380, 1000)
+    winsound.Beep(380, 500)
+    winsound.Beep(380, 500)
