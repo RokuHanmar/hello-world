86f89a22433241293432fc446830bb3c83d77154
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-12-31 10:22:13 +0000

    Create Hello World.py

---
+++ Winsound/Hello World.py
@@ -0,0 +1,23 @@
+import time
+from letters import *
+
+H()
+time.sleep(1)
+E()
+time.sleep(1)
+L()
+time.sleep(1)
+L()
+time.sleep(1)
+O()
+time.sleep(1)
+
+W()
+time.sleep(1)
+O()
+time.sleep(1)
+R()
+time.sleep(1)
+L()
+time.sleep(1)
+D()
