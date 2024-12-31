14eb8bc6e23cf9118cd9b8ade94dfc5b480e3034
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-12-31 09:17:22 +0000

    Update letters.py
    Fixed appearance of two turtles by making sure all letter functions used the turtle parameter

--- Turtle/letters.py
+++ Turtle/letters.py
@@ -28,10 +28,10 @@ def E(t):
         
     
 def L(t):
-    right(90)
-    forward(90)
-    left(90)
-    forward(30)
+    t.right(90)
+    t.forward(90)
+    t.left(90)
+    t.forward(30)
 
 def O(t):
     for i in range(2):
@@ -41,37 +41,36 @@ def O(t):
         t.right(90)
 
 def W(t):
-    right(90)
-    forward(90)
-    left(150)
-    forward(45)
-    right(130)
-    forward(45)
-    left(150)
-    forward(90)
+    t.right(90)
+    t.forward(90)
+    t.left(150)
+    t.forward(45)
+    t.right(130)
+    t.forward(45)
+    t.left(150)
+    t.forward(90)
 
 def R(t):
-    left(90)
-    forward(90)
+    t.left(90)
+    t.forward(90)
     for i in range(3):
-        right(90)
-        forward(30)
-    left(120)
-    forward(70)
+        t.right(90)
+        t.forward(30)
+    t.left(120)
+    t.forward(70)
 
 def D(t):
-    left(90)
-    forward(90)
-    right(140)
-    forward(70)
-    right(95)
-    forward(70)
+    t.left(90)
+    t.forward(90)
+    t.right(140)
+    t.forward(70)
+    t.right(95)
+    t.forward(70)
     
 def space(t):
     t.penup()
     t.setheading(90)
     t.forward(90)
     t.setheading(0)
-    t.forward(20)
+    t.forward(40)
     t.pendown()
-
