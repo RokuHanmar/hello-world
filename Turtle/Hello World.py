c211e887bf748d1c1db85766972ed3f3e8c6cea5
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-12-31 09:08:46 +0000

    Create Hello World.py
    Work in progress: creates a new turtle after E and then moves between them sporadically

---
+++ Turtle/Hello World.py
@@ -0,0 +1,26 @@
+from turtle import *
+from letters import *
+
+# s = Screen()
+t = Turtle()
+#x = 768
+#y = 768
+
+H(t)
+space(t)
+E(t)
+space(t)
+L(t)
+space(t)
+L(t)
+space(t)
+O(t)
+space(t)
+
+W(t)
+"""
+O(t)
+R(t)
+L(t)
+D(t)
+"""
